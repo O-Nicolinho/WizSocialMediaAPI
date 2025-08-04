c01f27cefb8e61502a9cd54208f60996d04253a3
Nicolinho <ritsuko_0@protonmail.com>
2025-08-03 20:53:30 -0400

    Init commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type server struct {
+	addr string
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("Index page"))
+			return
+		case "/users":
+			w.Write([]byte("users page"))
+			return
+		default:
+			w.Write([]byte("404 page"))
+		}
+	}
+}
+
+func main() {
+
+	s := &server{addr: ":8080"}
+	if err := http.ListenAndServe(s.addr, s); err != nil {
+		log.Fatal(err)
+	}
+}
