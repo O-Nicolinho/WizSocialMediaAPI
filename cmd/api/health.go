58d527d37b0de08f618dbe349ee675ed35ddf275
Nicolinho <ritsuko_0@protonmail.com>
2025-08-22 17:06:43 -0400

    DB work

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -5,6 +5,6 @@ import "net/http"
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 
-	app.store.Posts.Create(r.Context())
+	app.store.Posts.Create(r.Context(), nil)
 	// :
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/O-Nicolinho/WizSocialMediaAPI/internal/db"
 	"github.com/O-Nicolinho/WizSocialMediaAPI/internal/env"
 	"github.com/O-Nicolinho/WizSocialMediaAPI/internal/store"
 )
@@ -11,9 +12,21 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://user:adminpassword@localhost/social?// sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+		},
 	}
 
-	store := store.NewPostgresStorage(nil)
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewPostgresStorage(db)
 
 	app := &application{
 		config: cfg,
